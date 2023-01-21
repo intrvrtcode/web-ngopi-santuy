fddc7ecc5a3f52653182939cd7b8c13d456829b4
Aldi Rahmaddani <Kudahitamnfc12@gmail.com>
2023-01-21 14:34:09 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const hamburger = document.getElementById('hamburger-menu');
+const navbarNav = document.querySelector('.navbar-nav');
+
+// toggle class active 
+hamburger.addEventListener('click', () => {
+  navbarNav.classList.toggle('active');
+});
+
+// remove class actove
+document.addEventListener('click', (e) => {
+  if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+})
